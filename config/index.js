181f6dba02ac1ef986e29aa566435449488bad45
alxpez <alexperezdev@gmail.com>
2017-11-11 23:33:44 +1300

    - add proxy to devServer for comunication with backendServer - test a post to backend

--- config/index.js
+++ config/index.js
@@ -27,7 +27,12 @@ module.exports = {
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false
+      }
+    },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
--- src/components/PropertiesMenu.vue
+++ src/components/PropertiesMenu.vue
@@ -1,9 +1,27 @@
 <template>
-  <label>Props</label>
+  <button @click='generate()'>Generate</button>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import axios from 'axios'
+
 export default {
-  name: 'properties-menu'
+  name: 'properties-menu',
+  computed: mapState(['pages']),
+  methods: {
+    async generate () {
+      try {
+        await axios.post('/api/generate', {
+          body: this.pages
+        })
+        console.log('OK')
+        console.log(this.pages)
+      } catch (error) {
+        console.error('CHECK IF THE BACKEND SERVER IS UP AND RUNNING')
+        console.error(error)
+      }
+    }
+  }
 }
 </script>
