5efd8f47a950c605928ebc4601f9c3cde24a30c4
alxpez <alexperezdev@gmail.com>
2017-11-23 23:19:20 +1300

    refactor EggStage to include dynamic style/classes for pages #9 general clean up

--- src/screens/MainContainer.vue
+++ src/screens/MainContainer.vue
@@ -29,22 +29,4 @@ export default {
 .pageContainer {
   margin-top: 65px;
 }
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
