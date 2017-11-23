d9cf6015b007a1d7846868e32fa7df4c24eaf738
alxpez <alexperezdev@gmail.com>
2017-11-23 23:17:36 +1300

    Implement helpers #25

---
+++ src/helpers/page.js
@@ -0,0 +1,22 @@
+import shortid from 'shortid'
+
+// TODO: width and height should come dynamically
+function newPage (name, path) {
+  return {
+    id: shortid.generate(),
+    name: name,
+    path: path,
+    styles: {
+      'width': '1024px',
+      'height': '1000px',
+      'overflow': 'hidden',
+      'position': 'relative',
+      'margin': 'auto',
+      'background-color': '#fff'
+    },
+    classes: [],
+    children: []
+  }
+}
+
+export default newPage
