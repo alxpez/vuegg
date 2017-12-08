7d7bf8be4339b749225c6819242806f5405f9b13
alxpez <alexperezdev@gmail.com>
2017-12-09 09:06:42 +1300

    refactor mdl directive

--- src/directives/mdl.js
+++ src/directives/mdl.js
@@ -1,8 +1,18 @@
-// import Vue from 'vue'
-//
-// Vue.directive('mdl', {
-//   bind: function () {
-//     console.log(this.el)
-//     window.componentHandler.upgradeElement(this.el)
-//   }
-// })
+/**
+ * Vue Directive to apply MDL js functionality over vue components
+ *
+ * @type {Vue.directive}
+ * @see {@link https://vuejs.org/v2/guide/custom-directive.html|Vue Directives}
+ */
+const mdl = {
+  install (Vue) {
+    Vue.directive('mdl', {
+      bind: function (el) {
+        console.log('yoooo')
+        window.componentHandler.upgradeElement(el)
+      }
+    })
+  }
+}
+
+export default mdl
