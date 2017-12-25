c20a8200d539f11a3cd0de86071c88e772a4fa6a
alxpez <alexperezdev@gmail.com>
2017-12-25 22:43:51 +1300

    change vuegg-server port

--- config/index.js
+++ config/index.js
@@ -29,7 +29,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:5000',
         secure: false
       }
     },
