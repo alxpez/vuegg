49bb577c077f36f55684745df09b8bc3067f2a45
alxpez <alexperezdev@gmail.com>
2018-01-20 16:43:19 +1300

    add new svg-icons

---
+++ client/src/assets/icons/system/editor/corner_radius.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+
+var icon = require('vue-svgicon')
+icon.register({
+  'system/editor/corner_radius': {
+    width: 24,
+    height: 24,
+    viewBox: '0 0 24 24',
+    data: `<path pid="0" d="M19 19h2v2h-2v-2zm0-2h2v-2h-2v2zM3 13h2v-2H3v2zm0 4h2v-2H3v2zm0-8h2V7H3v2zm0-4h2V3H3v2zm4 0h2V3H7v2zm8 16h2v-2h-2v2zm-4 0h2v-2h-2v2zm4 0h2v-2h-2v2zm-8 0h2v-2H7v2zm-4 0h2v-2H3v2zM21 8a5 5 0 0 0-5-5h-5v2h5a3 3 0 0 1 3 3v5h2V8z"/>`
+  }
+})
---
+++ client/src/assets/icons/system/editor/flip_back.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+
+var icon = require('vue-svgicon')
+icon.register({
+  'system/editor/flip_back': {
+    width: 24,
+    height: 24,
+    viewBox: '0 0 24 24',
+    data: `<path pid="0" d="M9 7H7v2h2V7zm0 4H7v2h2v-2zm0-8a2 2 0 0 0-2 2h2V3zm4 12h-2v2h2v-2zm6-12v2h2a2 2 0 0 0-2-2zm-6 0h-2v2h2V3zM9 17v-2H7a2 2 0 0 0 2 2zm10-4h2v-2h-2v2zm0-4h2V7h-2v2zm0 8a2 2 0 0 0 2-2h-2v2zM5 7H3v12a2 2 0 0 0 2 2h12v-2H5V7zm10-2h2V3h-2v2zm0 12h2v-2h-2v2z"/>`
+  }
+})
