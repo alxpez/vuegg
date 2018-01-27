96057b9aa1b62456f7e4fed068fc3aed6248f11f
alxpez <alexperezdev@gmail.com>
2018-01-27 21:59:12 +1300

    implement oauth-config script to inject environment variables behind the scenes

---
+++ oauth-config.js
@@ -0,0 +1,27 @@
+const inquirer = require('inquirer')
+const shell = require('shelljs')
+
+require('dotenv').config()
+
+const setupQuestions = [
+  {
+    type: 'list',
+    name: 'runScript',
+    message: 'And what to run?',
+    choices: [
+      'client',
+      'server',
+      'start',
+      'vuegg'
+    ]
+  }
+]
+
+inquirer.prompt(setupQuestions).then(answers => {
+  const execCommand = 'CLIENT_ID='.concat(process.env.CLIENT_ID)
+    .concat(' CLIENT_SECRET=').concat(process.env.CLIENT_SECRET)
+    .concat(' CALLBACK_URL=').concat(process.env.CALLBACK_URL)
+    .concat(' npm run ').concat(answers.runScript)
+
+  shell.exec(execCommand)
+})
