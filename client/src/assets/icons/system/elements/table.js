ec53b502f654b1b72e4dfad44219202f3032fe31
alxpez <alexperezdev@gmail.com>
2018-02-14 21:51:28 +1300

    (chore) Add new svg-icons

---
+++ client/src/assets/icons/system/elements/table.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+
+var icon = require('vue-svgicon')
+icon.register({
+  'system/elements/table': {
+    width: 24,
+    height: 24,
+    viewBox: '0 0 24 24',
+    data: `<path pid="0" d="M20 2H4a2 2 0 0 0-2 2v16c0 1.1.9 2 2 2h16a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2zM8 20H4v-4h4v4zm0-6H4v-4h4v4zm0-6H4V4h4v4zm6 12h-4v-4h4v4zm0-6h-4v-4h4v4zm0-6h-4V4h4v4zm6 12h-4v-4h4v4zm0-6h-4v-4h4v4zm0-6h-4V4h4v4z"/>`
+  }
+})
